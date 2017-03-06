64f9c47721ec2080f88b2dbf1a28e755154307e1
lenn hypolite <lennhydesign@gmail.com>
2017-03-06 16:49:12 -0500

    added css feature to hover over image and fade background

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,18 +6,8 @@
     },
     updateYearOnPage: function(){
       document.getElementById("date").innerHTML = "© Polyverge "+myModule.year();
-    },
-    // makeReadMoreVissible: function(){
-    //     let el = document.getElementsByClassName("img-container");
-    //     for (let i=0;i<el.length;i++){
-    //       // console.log(el[i].childNodes[1]);
-    //       el[i].addEventListener("mouseover", function(){
-    //         el[i].childNodes[1].style.visibility = "visible";
-    //       }, false);
-    //     }
-    // }
+    }
   };
   myModule.updateYearOnPage();
-  // myModule.makeReadMoreVissible();
 
 })();
