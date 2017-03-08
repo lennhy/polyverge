514271df1abb5de5efdcab25ff3c8c1e3c85787d
lenn hypolite <lennhydesign@gmail.com>
2017-03-07 21:48:51 -0500

    Created a method to make container visible when user clicks the read me link of any of the several read me links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,32 @@
 "use strict";
 (function (){
-  var myModule = {
+  var module = {
     year: function(){
       return new Date().getFullYear();
     },
     updateYearOnPage: function(){
-      document.getElementById("date").innerHTML = "© Polyverge "+myModule.year();
+      document.getElementById("date").innerHTML = "© Polyverge "+module.year();
+    },
+    toggleContainer: function(element){
+      for (let i=0;i<element.length;i++){
+        element[i].addEventListener("click", function(){
+          let columnContainer = element[i].parentNode.parentNode.parentNode;
+          let jumbotronContainer = element[i].parentNode.parentNode.parentNode.querySelector(".jumbotron");
+          // replace visible class name with hidden class name and save it
+           jumbotronContainer.className = jumbotronContainer.className.replace(/visible/, 'hidden');
+           columnContainer.className = columnContainer.className + ' full-width';
+        }, false);
+      }
+    },
+    makeReadMoreVissible: function(){
+      let innerEl = document.querySelectorAll("ul.list-inline > li.read-more");
+      module.toggleContainer(innerEl);
     }
-  };
-  myModule.updateYearOnPage();
-
+  }
+  module.updateYearOnPage();
+  module.makeReadMoreVissible();
 })();
+
+
+// when link is clicked hidden box will turn to show and slide down
+// getElementById
