fc2e539d21369f11b4b85fa973aa8713bad2b512
lenn hypolite <lennhydesign@gmail.com>
2017-03-08 02:11:30 -0500

    Toggle function completed. Exit button completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,49 @@
 "use strict";
 (function (){
+
   var module = {
-    year: function(){
-      return new Date().getFullYear();
-    },
-    updateYearOnPage: function(){
-      document.getElementById("date").innerHTML = "© Polyverge "+module.year();
-    },
-    toggleContainer: function(element){
-      for (let i=0;i<element.length;i++){
-        element[i].addEventListener("click", function(){
-          let columnContainer = element[i].parentNode.parentNode.parentNode;
-          let jumbotronContainer = element[i].parentNode.parentNode.parentNode.querySelector(".jumbotron");
-          // replace visible class name with hidden class name and save it
-           jumbotronContainer.className = jumbotronContainer.className.replace(/visible/, 'hidden');
-           columnContainer.className = columnContainer.className + ' full-width';
-        }, false);
+
+      year: function(){
+          return new Date().getFullYear();
+      },
+
+      updateYearOnPage: function(){
+        document.getElementById("date").innerHTML = "© Polyverge "+module.year();
+      },
+
+      // Add new class-name to element
+      addClass: function(element, string){
+          element.className = element.className + string;
+      },
+
+      // Logic for toggling container
+      toggleAction: function(elements, div, oldAttribute, newAttribute){
+          for (let i=0;i<elements.length;i++){
+              elements[i].addEventListener("click", function(){
+                let columnContainer = elements[i].parentNode.parentNode.parentNode;
+                let jumbotronContainer = elements[i].parentNode.parentNode.parentNode.querySelector(div);
+                   jumbotronContainer.className = jumbotronContainer.className.replace(oldAttribute, newAttribute);
+                   module.addClass(columnContainer, ' full-width');
+              }, false);
+          }
+      },
+
+      selectInnerElements(innerEls){
+         let elements = document.querySelectorAll(innerEls);
+         return elements;
+      },
+      
+      // Call the action to toggle container
+      toggleContainer: function(){
+        module.toggleAction(module.selectInnerElements("ul.list-inline > li.read-more"), ".jumbotron", "hidden", "visible");
+        module.toggleAction(module.selectInnerElements("div.jumbotron .glyphicon-remove"), ".jumbotron", "visible", "hidden");
       }
-    },
-    makeReadMoreVissible: function(){
-      let innerEl = document.querySelectorAll("ul.list-inline > li.read-more");
-      module.toggleContainer(innerEl);
-    }
-  }
+
+  };
+
   module.updateYearOnPage();
-  module.makeReadMoreVissible();
+  module.toggleContainer();
+
 })();
 
 
