9167495002e58d472dd87f1065ef48a66ca9f37c
lenn hypolite <lennhydesign@gmail.com>
2017-03-08 23:11:36 -0500

    Styling portion of gallery completed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,9 +12,10 @@
       },
 
       // Logic for toggling container
-      toggleAction: function(elements, div, oldAttribute, newAttribute, selector){
+      toggleAction: function(elements, div, oldAttribute, newAttribute, selector, visibility){
           for (var i=0;i<elements.length;i++){
-            // debugger;
+
+            // Use an IIEF to pass the iterator in as a varible. This ensures that i's value is not changed by closure
             (function(x){
               elements[x].addEventListener("click", function(){
                 var columnContainer = elements[x].parentNode.parentNode.parentNode;
@@ -33,8 +34,8 @@
 
       // Call the action to toggle container
       toggleContainer: function(){
-        module.toggleAction(module.selectInnerElements("ul.list-inline > li.read-more"), ".jumbotron", "hidden", "visible", 'col-lg-3 col-md-4 col-xs-6 full-width');
-        module.toggleAction(module.selectInnerElements("ul.list-inline > .glyphicon-remove"), ".jumbotron", "visible", "hidden", "col-lg-3 col-md-4 col-xs-6");
+        module.toggleAction(module.selectInnerElements("ul.list-inline > li.read-more"), ".jumbotron", "hidden", "visible", 'col-lg-3 col-md-4 col-xs-6 full-width-absolute');
+        module.toggleAction(module.selectInnerElements("ul.list-inline > .glyphicon-remove"), ".jumbotron", "visible", "hidden", 'col-lg-3 col-md-4 col-xs-6');
       }
 
   };
@@ -43,58 +44,3 @@
   module.toggleContainer();
 
 })();
-// "use strict";
-// (function (){
-//   var module = {
-//     year: function(){
-//       return new Date().getFullYear();
-//     },
-//     updateYearOnPage: function(){
-//       document.getElementById("date").innerHTML = "© Polyverge "+module.year();
-//     },
-//     toggleContainerOpen: function(element){
-//       for (var i=0;i<element.length;i++){
-//         (function(x){
-//           element[x].addEventListener("click", function(){
-//           // console.log(i);
-//           var columnContainer = element[x].parentNode.parentNode.parentNode;
-//           var jumbotronContainer = element[x].parentNode.parentNode.parentNode.querySelector(".jumbotron");
-//           // replace visible class name with hidden class name and save it
-//            jumbotronContainer.className = jumbotronContainer.className.replace(/hidden/, 'visible');
-//            columnContainer.className = columnContainer.className + ' full-width';
-//         }, false);
-//       })(i);
-//       }
-//     },
-//     toggleContainerClose: function(button){
-//       for (var i=0;i<button.length;i++){
-//         // console.log(i);
-//         (function(x){
-//           button[x].addEventListener("click", function(){
-//
-//             var columnContainer = button[x].parentNode.parentNode.parentNode;
-//             var jumbotronContainer = button[x].parentNode.parentNode.parentNode.querySelector(".jumbotron");
-//             // replace visible class name with hidden class name and save it
-//              jumbotronContainer.className = jumbotronContainer.className.replace(/visible/, 'hidden');
-//              columnContainer.className = columnContainer.className ="col-lg-3 col-md-4 col-xs-6";
-//           }, false);
-//         })(i)
-//
-//       }
-//     },
-//
-//     makeReadMoreVissible: function(){
-//       var innerEl = document.querySelectorAll("ul.list-inline > li.read-more");
-//       var exitButton = document.querySelectorAll("ul.list-inline > .glyphicon-remove");
-//       module.toggleContainerOpen(innerEl);
-//       module.toggleContainerClose(exitButton);
-//     }
-//   }
-//   module.updateYearOnPage();
-//   module.makeReadMoreVissible();
-// })();
-
-
-// when link is clicked hidden box will turn to show and slide down
-// getElementById
-// Contact GitHub API Training Shop Blog About
