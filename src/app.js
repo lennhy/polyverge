e7b2b52cd3d45089260dc118102ce61b0940621c
lennhy <lennhydesign@gmail.com>
2018-05-31 19:09:26 -0400

    Deleted bower components. Add bootstrap 4, add scss loaders, turn css file into scss.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+import 'jquery'
+import 'bootstrap';
+import "bootstrap/scss/bootstrap.scss";
+import "../public/scss/_custom.scss";
 import './js/main.js';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
 const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
+    ]
   }
 };
